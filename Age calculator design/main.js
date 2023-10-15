7d64683549ddc93c17ff1dca078e9394aebc4a7c
unknown <augkinyi@gmail.com>
2023-10-15 23:53:01 +0300

    Second commit

---
+++ Age calculator design/main.js
@@ -0,0 +1,6 @@
+let day= document.querySelector("day");
+let month= document.querySelector("month");
+let year= document.querySelector("year");
+let year_output= document.querySelector("year-output");
+let month_output= document.querySelector("month-output");
+let day_output= document.querySelector("day-output");
